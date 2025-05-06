558891da88db4d4f5b81fb236ee0b88def85ae52
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-05-06 04:37:00 -0300

    Update Configuration.js
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

--- Configuration.js
+++ Configuration.js
@@ -3,14 +3,61 @@ function getConfiguration(config) {
 }
 
 function getEndpoints(deviceAddress, endpoints) {
-  var em = endpoints.addEndpoint("1","Battery Level", endpointType.genericSensor);
-  em.variableTypeId = 1006;
+  var em = endpoints.addEndpoint(
+    "1",
+    "Battery Level",
+    endpointType.genericSensor
+  );
+  em.variableTypeId = 1069;
 
-  endpoints.addEndpoint("2","Temperature sensor", endpointType.temperatureSensor);
+  endpoints.addEndpoint(
+    "2",
+    "Temperature sensor",
+    endpointType.temperatureSensor
+  );
 
   endpoints.addEndpoint("3", "Humidity sensor", endpointType.humiditySensor);
 
-  endpoints.addEndpoint("4","CO₂ sensor", endpointType.ppmConcentrationSensor,ppmConcentrationSensorSubType.carbonDioxide);
+  endpoints.addEndpoint(
+    "4",
+    "CO₂ sensor",
+    endpointType.ppmConcentrationSensor,
+    ppmConcentrationSensorSubType.carbonDioxide
+  );
+}
+
+function validateDeviceAddress(address, result) {
+  address = address.toLowerCase();
+  result.ok = true;
+  var validchars = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  for (var i = 0; i < address.length; i++) {
+    if (!validchars.includes(address.charAt(i))) {
+      result.ok = false;
+      break;
+    }
+  }
+  if (!result.ok)
+    result.errorMessage = {
+      en: "The address must be 16 characters long and only have hexadecimal characters",
+      es: "La dirección debe tener sólo caracteres hexadecimales",
+    };
 }
 
 function updateDeviceUIRules(device, rules) {
